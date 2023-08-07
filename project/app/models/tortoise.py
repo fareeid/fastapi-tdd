606e84a20282be809d505e2fbe1e32a93cbcae9c
Fareeid <faridkim@gmail.com>
2023-08-07 16:34:20 +0300

    Finalize part 1

--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -1,7 +1,10 @@
 # project/app/models/tortoise.py
 
-
+from datetime import datetime
 from tortoise import fields, models
+
+# from tortoise.contrib.pydantic import pydantic_model_creator
+from pydantic import BaseModel
 
 
 class TextSummary(models.Model):
@@ -11,3 +14,16 @@ class TextSummary(models.Model):
 
     def __str__(self):
         return self.url
+
+
+class SummarySchema(BaseModel):
+    id: int
+    url: str
+    summary: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+# SummarySchema = pydantic_model_creator(TextSummary)
